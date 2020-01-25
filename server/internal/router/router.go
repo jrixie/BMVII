8dceb0bcbfae0a927e9b9014c60b4218fa53d9eb
Andrew Davis <drew@drewdavis.me>
2020-01-25 15:50:25 -0500

    Add logging to router

--- server/internal/router/router.go
+++ server/internal/router/router.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 	"net/http"
 	"strings"
+	"time"
 
 	"boilermakevii/api/internal/test"
 
 	"github.com/gorilla/mux"
+	log "github.com/sirupsen/logrus"
 )
 
 // NewRouter constructs a new router for the API to route requests.
@@ -40,8 +42,10 @@ func NewRouter() (router *mux.Router) {
 // apiMiddleware defines a common middleware used across all endpoints
 func apiMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
 		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 		next.ServeHTTP(w, r)
+		log.Info(fmt.Sprintf("%s\t%s\tin %v", r.Method, r.URL.Path, time.Since(start)))
 	})
 }
 
